da6832b4e25a4ecbc83f0c4c3b1ed3dd1e82c729
deepester <deepest.lee@gmail.com>
2024-10-04 07:51:18 +0900

    [Modify] tab

--- components/page/Tpyetab.vue
+++ components/page/Tpyetab.vue
@@ -12,7 +12,14 @@
         </span>
       </li>
     </ul>
-    <component :is="activeComponent" />
+    <div class="selbox">
+      <label for="">상품분류</label>
+      <select v-model="selectedOption" @change="handleSelectChange">
+        <option v-for="subTab in activeSubMenu" :key="subTab.name" :value="subTab.name">
+          {{ subTab.label }}
+        </option>
+      </select>
+    </div>
   </div>
   <section>
     <component :is="activeComponent" />
@@ -92,5 +99,40 @@ const activeComponent = computed(() => {
       }
     }
   }
+}
+
+.selbox {
+    width: 28%;
+    background: #f5f5f5;
+    border-radius: 0 .4rem .4rem 0;
+    height: 3em;
+    padding: .25rem .25rem 0 1rem;
+
+    label {
+        position: relative;
+        padding-left: 30px;
+        margin-right: 10px;
+        &::before {
+            position: absolute;
+            content: "devices";
+            width: 30px;
+            font-size: 1.5rem;
+            display: block;
+            font-family: "material icons";
+        }
+    }
+    select {
+        user-select: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        cursor: pointer;
+        padding: 0 .5rem;
+        font-size: 1rem; 
+        border-radius: 0;
+        height: 32px;
+        line-height: 32px;
+        padding-right: 3rem;
+    }
 }
 </style>
--- pages/guide/charges/Charges.vue
+++ pages/guide/charges/Charges.vue
@@ -4,17 +4,41 @@
 
 <script setup lang="ts">
 import Basic from "./service/Basic.vue";
+import Backbone from "./service/Backbone.vue";
 
 const tabs = [
-  { name: "service", label: "서비스정보", component: Basic},
-  { name: "service1", label: "서비스정보", component: Basic},
-  { name: "service2", label: "서비스정보", component: Basic},
+  {
+    name: "service",
+    label: "전용회선서비스",
+    component: Basic,
+    subMenu: [
+      { name: "service1", label: "기본회선", component: Basic },
+      { name: "service2", label: "백본회선", component: Backbone },
+    ],
+  },
+  {
+    name: "service1",
+    label: "인터넷 서비스",
+    component: Basic,
+    subMenu: [
+      { name: "service1", label: "인터넷서비스", component: Basic },
+    ],
+  },
+  {
+    name: "service2",
+    label: "인터넷 전화 서비스",
+    component: Basic,
+    subMenu: [
+      { name: "service1", label: "기본회선", component: Basic },
+      { name: "service2", label: "백본회선", component: Backbone },
+    ],
+  },
 ];
 </script>
 <script lang="ts">
 export default {
-  name: 'Basic'
-}
+  name: "Basic",
+};
 </script>
 
 <style lang="scss" scoped></style>
